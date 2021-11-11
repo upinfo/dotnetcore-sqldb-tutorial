fa68a2e9014c87365269e29a671c28aee9a59072
Morlu Holder <morluholder2@gmail.com>
2021-11-10 23:07:13 -0500

    add db connection to azure

--- Startup.cs
+++ Startup.cs
@@ -28,8 +28,12 @@ namespace DotNetCoreSqlDb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddDbContext<MyDatabaseContext>(options =>
-                    options.UseSqlite("Data Source=localdatabase.db"));
+if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+        services.AddDbContext<MyDatabaseContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
+else
+        services.AddDbContext<MyDatabaseContext>(options =>
+                 options.UseSqlite("Data Source=MvcMovie.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
